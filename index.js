958d6fa9f966caa4c218c999ce0ff1c626cee376
Christian Smitherman <104788550+christian-js@users.noreply.github.com>
2023-04-14 12:32:23 -0600

    Created prime functions

---
+++ index.js
@@ -0,0 +1,78 @@
+export const isPrime = (num) => {
+  if (num < 0) {
+    for (let i = -2; i >= num / 2; i--) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  } else {
+    for (let i = 2; i <= num / 2; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+export const getPrimes = (max, start) => {
+  try {
+    if (!start) {
+      if (max < 0) {
+        start = -1;
+      } else {
+        start = 1;
+      }
+    }
+    const primes = [];
+    if (max < 0) {
+      for (let i = start; i >= max; i--) {
+        if (isPrime(i)) {
+          primes.push(i);
+        }
+      }
+    } else {
+      for (let i = start; i <= max; i++) {
+        if (isPrime(i)) {
+          primes.push(i);
+        }
+      }
+    }
+
+    return primes;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getPrimesAsObjects = (max, start) => {
+  try {
+    if (!start) {
+      if (max < 0) {
+        start = -1;
+      } else {
+        start = 1;
+      }
+    }
+    const primes = [];
+    if (max < 0) {
+      for (let i = start; i >= max; i--) {
+        primes.push({
+          value: i,
+          isPrime: isPrime(i),
+        });
+      }
+    } else {
+      for (let i = start; i <= max; i++) {
+        primes.push({
+          value: i,
+          isPrime: isPrime(i),
+        });
+      }
+    }
+    return primes;
+  } catch (e) {
+    console.log(e);
+  }
+};
